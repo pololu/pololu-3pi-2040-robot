d1cc7a5c1bf75a0968e93379a95f4b21d4daf35d
David (Pololu) <dev-david@pololu.com>
2023-02-05 11:51:32 -0800

    Improvements to ir_sensors.c.

--- c/pololu_3pi_plus_2040_robot/ir_sensors.c
+++ c/pololu_3pi_plus_2040_robot/ir_sensors.c
@@ -1,3 +1,5 @@
+// Copyright (C) Pololu Corporation.  See LICENSE.txt for details.
+
 #include <stdio.h>  // tmphax
 #include <string.h>
 
@@ -23,11 +25,15 @@ void ir_sensors_run()
     counter_sm = pio_claim_unused_sm(IR_PIO, true);
   }
 
-  // Put the emitters in the desired state.
-  bool emitters_on = true;
+  // Turn on the line sensor emitter.
   gpio_init(26);
-  gpio_put(26, emitters_on);
+  gpio_put(26, true);
   gpio_set_dir(26, GPIO_OUT);
+
+  // Turn off the bump sensor emitter.
+  gpio_init(23);
+  gpio_put(23, false);
+  gpio_set_dir(23, GPIO_OUT);
 
   const uint32_t mask = 0x7F << 16;
 
@@ -59,11 +65,18 @@ void ir_sensors_run()
   sm_config_set_in_pins(&cfg, 16);
   sm_config_set_out_pins(&cfg, 16, 7);
   sm_config_set_fifo_join(&cfg, PIO_FIFO_JOIN_RX);
-  sm_config_set_in_shift(&cfg, false, true, 32);
+  sm_config_set_in_shift(&cfg, false, true, 23);
   pio_sm_init(IR_PIO, counter_sm, counter_offset, &cfg);
 
   pio_sm_clear_fifos(IR_PIO, counter_sm);
   pio_sm_restart(IR_PIO, counter_sm);
+
+  // Set the counter (y) to 1023 (10 ones).
+  pio_sm_exec_wait_blocking(IR_PIO, counter_sm, 0xA0EB);  // mov osr, !null
+  pio_sm_exec_wait_blocking(IR_PIO, counter_sm, 0x604A);  // out y, 10
+
+  pio_sm_exec_wait_blocking(IR_PIO, counter_sm, 0xA0E3);  // mov osr, null
+
   pio_sm_set_enabled(IR_PIO, counter_sm, true);
 
   memset(ir_sensor_values, 0, sizeof(ir_sensor_values));
@@ -72,11 +85,11 @@ void ir_sensors_run()
   while (1)
   {
     uint32_t data = pio_sm_get_blocking(IR_PIO, counter_sm);
-    // printf("IR %08lx us: %08lx\n", time_us_32(), value);
+    //printf("IR %08lx us: %08lx\n", time_us_32(), data);
     if (data == 0xFFFFFFFF) { break; }
 
-    uint16_t time = data >> 16;
-    uint8_t state = data & 0x7F;
+    uint16_t time = data;
+    uint8_t state = data >> 16 & 0x7F;
     uint8_t new_zeros = last_state & ~state;
     if (new_zeros & (1 << 0)) { ir_sensor_values[0] = time; }
     if (new_zeros & (1 << 1)) { ir_sensor_values[1] = time; }
@@ -90,5 +103,7 @@ void ir_sensors_run()
 
   pio_sm_set_enabled(IR_PIO, counter_sm, false);  // stop the state machine
 
-  gpio_put(26, 0); // turn off emitters
+  // turn off emitters
+  gpio_put(26, 0);
+  gpio_put(23, 0);
 }
