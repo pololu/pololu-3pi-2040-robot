b8a52b9c9f1d317703677368bd13c393ba1b2504
Paul (Pololu) <dev-paul@pololu.com>
2023-02-17 21:13:05 -0800

    micropython imu test: show values on display

--- micropython_demo/imu_test.py
+++ micropython_demo/imu_test.py
@@ -1,6 +1,7 @@
 import pololu_3pi_plus_2040_robot as robot
 import time
 
+display = robot.Display()
 imu = robot.IMU()
 imu.reset()
 imu.enable_default()
@@ -14,4 +15,14 @@ while True:
     a = imu.acc.last_reading_g
     m = imu.mag.last_reading_gauss
     print(f"G: {format_reading(g, '6.1f')}    A: {format_reading(a, '6.3f')}    M: {format_reading(m, '6.3f')}")
+    
+    display.fill(0)
+    display.text("gyro (dps):", 0, 0)
+    display.text("{:>5.1f} {:>5.1f}{:>5.1f}".format(*g), 0, 10)
+    display.text("acc (g):", 0, 23)
+    display.text("{:>5.2f} {:>5.2f}{:>5.2f}".format(*a), 0, 33)
+    display.text("mag (gauss):", 0, 47)
+    display.text("{:>5.2f} {:>5.2f}{:>5.2f}".format(*m), 0, 57)
+    display.show()
+    
     time.sleep_ms(100)
