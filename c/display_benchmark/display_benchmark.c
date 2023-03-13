f53b109d388646eec35bfa1dafb3fa30a3e4eb5f
David (Pololu) <dev-david@pololu.com>
2023-03-12 22:04:04 -0700

    display_benchmark.c: Fixed the benchmarks which claimed to be doing 16-characters of unicode but only did 15.
    Added benchmarks for filling the screen with text.
    
    Benchmark results
    Clear:                                  708
    8x16: 4-char ASCII update:              88
    8x16: 16-char ASCII update:             227
    8x16: 16-char Unicode update:           260
    8x16: 16-char Unicode render:           72
    8x16: full update:                      879
    8x8: 4-char ASCII update:               56
    8x8: 16-char ASCII update:              142
    8x8: 16-char Unicode update:            178
    8x8: 16-char Unicode render:            49
    8x8: full update:                       953

--- c/display_benchmark/display_benchmark.c
+++ c/display_benchmark/display_benchmark.c
@@ -40,8 +40,6 @@ int main()
 
     display_set_font(font_8x16);
 
-    // TODO: 8x16 full update
-
     start = time_us_32();
     display_text("hi:)", 0, 0, DISPLAY_NOW);
     report(time_us_32() - start, "8x16: 4-char ASCII update");
@@ -51,18 +49,25 @@ int main()
     report(time_us_32() - start, "8x16: 16-char ASCII update");
 
     start = time_us_32();
-    display_text("°±²µΔΘΩθμπ…←↑→𝟅", 4, 24, DISPLAY_NOW);
+    display_text("°±²µΔΘΩθμπ…←↑→☺𝟅", 0, 24, DISPLAY_NOW);
     report(time_us_32() - start, "8x16: 16-char Unicode update");
 
     start = time_us_32();
-    display_text("°±²µΔΘΩθμπ…←↑→𝟅", 4, 24, 0);
+    display_text("°±²µΔΘΩθμπ…←↑→☺𝟅", 0, 24, 0);
     report(time_us_32() - start, "8x16: 16-char Unicode render");
+
+    start = time_us_32();
+    display_fill(0);
+    display_text("Full update!    ", 0, 0, 0);
+    display_text("Tall 8x16 font  ", 0, 16, 0);
+    display_text("& some unicode  ", 0, 32, 0);
+    display_text("°±²µΔΘΩθμπ…←↑→☺𝟅", 0, 48, 0);
+    display_show();
+    report(time_us_32() - start, "8x16: full update");
 
     display_set_font(font_8x8);
     display_fill(0);
     display_show();
-
-    // TODO: 8x8 full update
 
     start = time_us_32();
     display_text("hi:)", 0, 0, DISPLAY_NOW);
@@ -73,11 +78,24 @@ int main()
     report(time_us_32() - start, "8x8: 16-char ASCII update");
 
     start = time_us_32();
-    display_text("°±²µΔΘΩθμπ…←↑→𝟅", 4, 8, DISPLAY_NOW);
+    display_text("°±²µΔΘΩθμπ…←↑→𝟅", 0, 8, DISPLAY_NOW);
     report(time_us_32() - start, "8x8: 16-char Unicode update");
 
     start = time_us_32();
-    display_text("°±²µΔΘΩθμπ…←↑→𝟅", 4, 8, 0);
+    display_text("°±²µΔΘΩθμπ…←↑→𝟅", 0, 8, 0);
     report(time_us_32() - start, "8x8: 16-char Unicode render");
+
+    start = time_us_32();
+    display_fill(0);
+    display_text("Full update!    ", 0, 0, 0);
+    display_text("Short 8x8 font  ", 0, 8, 0);
+    display_text("& some unicode  ", 0, 16, 0);
+    display_text("°±²µΔΘΩθμπ…←↑→☺𝟅", 0, 24, 0);
+    display_text("Important for   ", 0, 32, 0);
+    display_text("people who want ", 0, 40, 0);
+    display_text("lots of data!   ", 0, 48, 0);
+    display_text("☺☻♡♥☺☻♡♥☺☻♡♥☺☻♡♥", 0, 56, 0);
+    display_show();
+    report(time_us_32() - start, "8x8: full update");
   }
 }
