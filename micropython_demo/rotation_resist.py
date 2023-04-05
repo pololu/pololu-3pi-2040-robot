7e74541d32739a1a386eb3783187f0a5fb21c173
David (Pololu) <dev-david@pololu.com>
2023-04-05 16:31:58 -0700

    rotation_resist.py: Clean up the code.

--- micropython_demo/rotation_resist.py
+++ micropython_demo/rotation_resist.py
@@ -1,18 +1,10 @@
-# TODO: fix gyro sensitivtity so angle doesn't get messed up
 # TODO: tune the PID parameters for standard edition
 # TODO: make it work well on the other editions too (with a menu to select them?)
-# TODO: clean up the code
 
-# TODO: why does the angle get exteremely messed up if you pick up the robot, turn
-# it, and put it down again?  I bet the gyro is saturating, so we need to pick
-# better settings.
-
-import time
 from pololu_3pi_2040_robot import robot
+import time
 
-#rgb_leds = robot.RGBLEDs()
 motors = robot.Motors()
-#buzzer = robot.Buzzer()
 button_a = robot.ButtonA()
 display = robot.Display()
 yellow_led = robot.YellowLED()
@@ -24,42 +16,55 @@ imu.enable_default()
 display.fill(0)
 display.text("Calibrating...", 0, 0, 1)
 display.show()
+time.sleep_ms(500)   # skip spurious readings at startup
 start = time.ticks_ms()
 stationary_gz = 0.0
 reading_count = 0
 while time.ticks_diff(time.ticks_ms(), start) < 3000:
-    imu.read()
-    stationary_gz += imu.gyro.last_reading_dps[2]
-    reading_count += 1
+    if imu.gyro.data_ready():
+        imu.gyro.read()
+        stationary_gz += imu.gyro.last_reading_dps[2]
+        reading_count += 1
 stationary_gz /= reading_count
 
 drive_motors = False
 last_time = None
 angle = 0.0
+
+def draw_options():
+  display.fill(0)
+  a = "A: Stop motors" if drive_motors else "A: Start motors"
+  display.text(a, 0, 0, 1)
+
+draw_options()
+
 while True:
     # Update the angle and the turn rate.
-    imu.read()
-    turn_rate = imu.gyro.last_reading_dps[2] - stationary_gz  # degrees per second
-    now = time.ticks_us()
-    if last_time:
-      dt = time.ticks_diff(now, last_time) / 1000000
-      angle += turn_rate * dt
+    if imu.gyro.data_ready():
+        imu.gyro.read()
+        turn_rate = imu.gyro.last_reading_dps[2] - stationary_gz  # degrees per second
+        now = time.ticks_us()
+        if last_time:
+            dt = time.ticks_diff(now, last_time)
+            angle += turn_rate * dt / 1000000
+        last_time = now
 
     # If the user presses button A, toggle whether the motors are on.
     if button_a.check() == True:
         drive_motors = not drive_motors
         if drive_motors:
-            display.fill(0)
-            display.text("Starting motors!".format(angle), 0, 0, 1)
+            display.fill(1)
+            display.text("Spinning", 30, 20, 0)
+            display.text("WATCH OUT", 27, 30, 0)
             display.show()
             time.sleep_ms(500)
-            last_time = time.ticks_us()
+        draw_options()
+        last_time = time.ticks_us()
 
-    # Update the display
-    display.fill(0)
-    display.text("{:>5.1f}".format(angle), 0, 0, 1)
+    # Show the current angle in degrees.
+    display.fill_rect(0, 32, 72, 8, 0)
+    display.text("{:>9.3f}".format(angle), 0, 32, 1)
     display.show()
-    last_time = now
 
     # Drive motors
     if drive_motors:
@@ -71,23 +76,3 @@ while True:
     else:
         motors.off()
         yellow_led.off()
-
-# for i in range(0, max, step):
-#     motors.set_speeds(i, -i)
-#     time.sleep_ms(10)
-
-# time.sleep_ms(500)
-
-# for i in range(max, 0, -step):
-#     motors.set_speeds(i, -i)
-#     time.sleep_ms(15)
-
-# for i in range(0, -max, -step):
-#     motors.set_speeds(i, -i)
-#     time.sleep_ms(15)
-
-# time.sleep_ms(500)
-
-# for i in range(-max, 0, step):
-#     motors.set_speeds(i, -i)
-#     time.sleep_ms(10)
