148b6edfb029988a191966a7a183619f9bcac419
David (Pololu) <dev-david@pololu.com>
2023-04-05 18:24:28 -0700

    rotation_resist.py: Added data logging and good PID parameters.

--- micropython_demo/rotation_resist.py
+++ micropython_demo/rotation_resist.py
@@ -6,12 +6,18 @@ import time
 
 motors = robot.Motors()
 button_a = robot.ButtonA()
+button_c = robot.ButtonC()
 display = robot.Display()
 yellow_led = robot.YellowLED()
 
 imu = robot.IMU()
 imu.reset()
 imu.enable_default()
+
+# Standard edition motion parameters:
+max_speed = 3000
+kp = 160
+kd = 4
 
 display.fill(0)
 display.text("Calibrating...", 0, 0, 1)
@@ -29,14 +35,22 @@ stationary_gz /= reading_count
 
 drive_motors = False
 last_time = None
+turn_rate = 0
 angle = 0.0
+log = None
+log_start_time = None
 
-def draw_options():
+def log_time():
+    return time.ticks_diff(time.ticks_us(), log_start_time)
+
+def draw_text():
   display.fill(0)
   a = "A: Stop motors" if drive_motors else "A: Start motors"
   display.text(a, 0, 0, 1)
+  if log: display.text("Logging", 0, 48, 1)
+  display.text(f"{kp}, {kd}", 0, 56, 1)
 
-draw_options()
+draw_text()
 
 while True:
     # Update the angle and the turn rate.
@@ -48,6 +62,7 @@ while True:
             dt = time.ticks_diff(now, last_time)
             angle += turn_rate * dt / 1000000
         last_time = now
+        if log: print(f"{log_time()},{angle:.3f}", file=log)
 
     # If the user presses button A, toggle whether the motors are on.
     if button_a.check() == True:
@@ -58,21 +73,31 @@ while True:
             display.text("WATCH OUT", 27, 30, 0)
             display.show()
             time.sleep_ms(500)
-        draw_options()
+        draw_text()
         last_time = time.ticks_us()
+
+    if button_c.check() == True:
+        if log:
+            log.close()
+            log = None
+        else:
+            log = open("rotation.log", "w")
+            log_start_time = time.ticks_us()
+        draw_text()
 
     # Show the current angle in degrees.
     display.fill_rect(0, 32, 72, 8, 0)
-    display.text("{:>9.3f}".format(angle), 0, 32, 1)
+    display.text(f"{angle:>9.3f}", 0, 32, 1)
     display.show()
 
-    # Drive motors
+    # Drive motors.
     if drive_motors:
-        speed = angle / 30 * motors.MAX_SPEED + turn_rate / 50
-        if speed > 3000: speed = 3000
-        if speed < -3000: speed = -3000
+        speed = angle * kp + turn_rate * kd
+        if speed > max_speed: speed = max_speed
+        if speed < -max_speed: speed = -max_speed
         motors.set_speeds(speed, -speed)
         yellow_led.on()
     else:
         motors.off()
         yellow_led.off()
+
