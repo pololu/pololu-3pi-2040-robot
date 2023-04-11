e80af9c01347c92c014ad787ecaddebba506ef42
David (Pololu) <dev-david@pololu.com>
2023-04-10 17:52:45 -0700

    face_uphill.py rewrite: Use the same PD algorithm as rotation_resist.py, just update the target angle to make it face uphill.

--- micropython_demo/face_uphill.py
+++ micropython_demo/face_uphill.py
@@ -21,36 +21,32 @@ display.show()
 imu = robot.IMU()
 imu.reset()
 imu.enable_default()
-#imu.acc.set_output_data_rate(12.5)  # TODO: needed?
 
 edition = editions.select()
-
-# 12.5 Hz ODR: oscillation happened around 16000, with period of 0.25 s.
-# Regular ODS: oscillation happened at 25000, with period of ~0.125 s
-if edition == "Standard":   # TODO: tune
+if edition == "Standard":
     max_speed = 3000
+    kp = 160
+    kd = 4
     ke = 15
-    kp = 20000
-    # TODO: kp = 12800, kd = 500
 elif edition == "Turtle":   # TODO: tune
     max_speed = 6000
+    kp = 200
+    kd = 0
     ke = 15
-    kp = 8000
 elif edition == "Hyper":    # TODO: tune
     motors.flip_left(True)
     motors.flip_right(True)
     max_speed = 1500
+    kp = 200
+    kd = 0
     ke = 15
-    kp = 8000
 
-last_display_time = 0
-log = None
-log_start_time = None
-x = 0
-y = 0
-gz = 0
-last_acc_read_time = 0
-sy = 0
+drive_motors = False
+last_time_gyro_reading = None
+turn_rate = 0.0    # degrees per second
+robot_angle = 0.0  # degrees
+target_angle = 0.0
+tilted = False
 
 def constrain_speed(speed):
   if speed < -max_speed: return -max_speed
@@ -59,80 +55,83 @@ def constrain_speed(speed):
 
 def draw_text():
   display.fill(0)
-  display.text(f"x:", 0, 24, 1)
-  display.text(f"y:", 0, 32, 1)
-  display.text(f"e:", 0, 40, 1)
-  if log: display.text("Logging", 0, 48, 1)
+  if drive_motors:
+        display.text("A: Stop motors", 0, 0, 1)
+  else:
+        display.text("A: Start motors", 0, 0, 1)
+  display.text(f"ay:", 0, 24, 1)
+  display.text(f"Robot:", 0, 32, 1)
+  display.text(f"Target:", 0, 40, 1)
+  display.text(f"Enc:", 0, 48, 1)
   display.text(edition, 0, 56, 1)
-
-def log_time():
-    return time.ticks_diff(time.ticks_us(), log_start_time)
 
 draw_text()
 display.show()
-
-time.sleep_ms(500)  # Delay before running motors.
+time.sleep_ms(500)
 
 while True:
-    # Read the accelerometer and encoders.
-    if imu.acc.data_ready():
-        imu.acc.read()
-        dt = time.ticks_diff(time.ticks_us(), last_acc_read_time)
-        sy = (imu.acc.last_reading_g[1] - y) * 1000000 / dt
-        x = imu.acc.last_reading_g[0]
-        y = imu.acc.last_reading_g[1]
-        encoder_counts = sum(encoders.get_counts())
-        if log: print(f"{log_time()},{x:.3f},{y:.3f},{sy:.4f},{gz:.3f}", file=log)
-        last_acc_read_time = time.ticks_us()
+    encoder_counts = sum(encoders.get_counts())
 
     if imu.gyro.data_ready():
+        # Read the gyroscope and accelerometer.
         imu.gyro.read()
-        gz = imu.gyro.last_reading_dps[2]
+        imu.acc.read()
 
-    # If the user pressed button C, toggle data logging.
-    if button_c.check() == True:
-        if log:
-            log.close()
-            log = None
-        else:
-            log = open("face_uphill.log", "w")
-            log_start_time = time.ticks_us()
+        # Update the robot's angle.
+        turn_rate = imu.gyro.last_reading_dps[2]
+        now = time.ticks_us()
+        if last_time_gyro_reading:
+            dt = time.ticks_diff(now, last_time_gyro_reading)
+            robot_angle += turn_rate * dt / 1000000
+        last_time_gyro_reading = now
+
+        # Detect if the robot is tilted more than 5 degrees.
+        # If so, adjust the target angle slowly towards the uphill direction.
+        ax = imu.acc.last_reading_g[0]
+        ay = imu.acc.last_reading_g[1]
+        tilted = ax * ax + ay * ay > 0.007596  # sin(5 deg)^2 = 0.007596
+        if tilted:
+            angle_increment = 0.8 if abs(ay > 0.1) or ax < 0 else 0.4
+            if ay < 0:
+                target_angle -= angle_increment
+            else:
+                target_angle += angle_increment
+
+    # If the user presses button A, toggle whether the motors are on.
+    if button_a.check() == True:
+        while button_a.check() != False: pass  # wait for release
+        drive_motors = not drive_motors
+        if drive_motors:
+            display.fill(1)
+            display.text("Spinning", 30, 20, 0)
+            display.text("WATCH OUT", 27, 30, 0)
+            display.show()
+            time.sleep_ms(500)
         draw_text()
+        encoders.get_counts(reset=True)
+        target_angle = robot_angle
+        last_time_gyro_reading = time.ticks_us()
 
-    # Display sensor readings on the OLED every 150 ms.
-    if time.ticks_diff(time.ticks_ms(), last_display_time) > 150:
-        last_display_time = time.ticks_ms()
-        display.fill_rect(32, 24, 48, 24, 0)
-        display.text(f"{x:>6.3f}", 32, 24, 1)
-        display.text(f"{y:>6.3f}", 32, 32, 1)
-        display.text(f"{encoder_counts:>5}", 32, 40, 1)
-        display.show()
+    # Show the current angle in degrees.
+    display.fill_rect(48, 24, 72, 8 * 4, 0)
+    display.text(f"{ay:>9.3f}",           48, 24, 1)
+    display.text(f"{robot_angle:>9.3f}",  48, 32, 1)
+    display.text(f"{target_angle:>9.3f}", 48, 40, 1)
+    display.text(f"{encoder_counts:>5}",  48, 48, 1)
+    display.show()
 
-    # sin(5 deg)^2 = 0.007596
-    if x * x + y * y > 0.007596:
-        # We are on an incline of more than 5 degrees.
-        if x < 0:
-          # We are more than 90 degrees away from uphill, so just turn at
-          # the maximum speed.
-          if y < 0:
-            turn_speed = -max_speed
-          else:
-            turn_speed = max_speed
-        else:
-          # We are within 90 degrees of facing uphill, so use a feedback
-          # algorithm to face uphill.
-          turn_speed = y * kp
+    if drive_motors:
+        # Turn towards the target angle.
+        turn_speed = (target_angle - robot_angle) * kp - turn_rate * kd
 
-        yellow_led.on()
+        # Avoid rolling downhill.
+        forward_speed = constrain_speed(-encoder_counts * ke)
+
+        left_speed = constrain_speed(forward_speed - turn_speed)
+        right_speed = constrain_speed(forward_speed + turn_speed)
+        motors.set_speeds(left_speed, right_speed)
     else:
-        # We are not on a noticeable incline, so don't turn.
-        turn_speed = 0
-        yellow_led.off()
+        motors.off()
 
-    # If the robot rolls downhill, the encoder counts will become negative,
-    # resulting in a positive forward_speed to counteract the rolling.
-    forward_speed = constrain_speed(-encoder_counts * ke)
+    yellow_led.value(drive_motors)
 
-    left_speed = constrain_speed(forward_speed - turn_speed)
-    right_speed = constrain_speed(forward_speed + turn_speed)
-    motors.set_speeds(left_speed, right_speed)
