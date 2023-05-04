3fba883ab558a92b7cadf08d85084e12b4e24a19
David (Pololu) <dev-david@pololu.com>
2023-05-04 15:33:52 -0700

    motor_test.py: Don't specify text color explicitly.

--- micropython_demo/motor_test.py
+++ micropython_demo/motor_test.py
@@ -16,12 +16,12 @@ button_c = robot.ButtonC()
 motors = robot.Motors()
 encoders = robot.Encoders()
 
-display.text("Hold=run", 32, 0, 1)
-display.text("Tap=flip", 32, 8, 1)
-display.text("A", 8, 28, 1)
-display.text("C", 112, 28, 1)
-display.text("L: ", 24, 48, 1)
-display.text("R: ", 24, 56, 1)
+display.text("Hold=run", 32, 0)
+display.text("Tap=flip", 32, 8)
+display.text("A", 8, 28)
+display.text("C", 112, 28)
+display.text("L: ", 24, 48)
+display.text("R: ", 24, 56)
 
 arrows = ["v", None, "^"]
 left_dir = 1
@@ -68,15 +68,15 @@ while True:
 
         display.fill_rect(0, 0, 8, 64, 0)
         y = 28 + -left_dir * left_speed // 225
-        display.text(arrows[left_dir + 1], 0, y, 1)
+        display.text(arrows[left_dir + 1], 0, y)
 
         display.fill_rect(120, 0, 8, 64, 0)
         y = 28 + -right_dir * right_speed // 225
-        display.text(arrows[right_dir + 1], 120, y, 1)
+        display.text(arrows[right_dir + 1], 120, y)
 
         display.fill_rect(40, 48, 64, 16, 0)
         left_encoder, right_encoder = encoders.get_counts()
-        display.text(f"{left_encoder:>8}", 40, 48, 1)
-        display.text(f"{right_encoder:>8}", 40, 56, 1)
+        display.text(f"{left_encoder:>8}", 40, 48)
+        display.text(f"{right_encoder:>8}", 40, 56)
 
         display.show()
