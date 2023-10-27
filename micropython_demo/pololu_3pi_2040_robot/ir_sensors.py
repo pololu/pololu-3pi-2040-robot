7bb3c5ed873fb119a117c8ef2b38abfcd7823c35
Kevin (Pololu) <dev-kevin@pololu.com>
2023-10-26 20:10:29 -0700

    ir_sensors: renamed again to SENSOR_COUNT

--- micropython_demo/pololu_3pi_2040_robot/ir_sensors.py
+++ micropython_demo/pololu_3pi_2040_robot/ir_sensors.py
@@ -5,7 +5,7 @@ import time
 import rp2
 from rp2 import PIO
 
-NUM_SENSORS = const(7)
+SENSOR_COUNT = const(7)
 TIMEOUT = const(1024)
 _DONE = const(0)
 _READ_LINE = const(1)
@@ -16,20 +16,20 @@ _qtr = None
 class QTRSensors:
     """A multi-channel QTR sensor reader using PIO"""
     @rp2.asm_pio(
-        out_init=(PIO.OUT_HIGH,) * NUM_SENSORS,
+        out_init=(PIO.OUT_HIGH,) * SENSOR_COUNT,
         autopush=True, # saves push instructions
-        push_thresh=NUM_SENSORS + 16,
+        push_thresh=SENSOR_COUNT + 16,
         fifo_join=PIO.JOIN_RX
         )
     def counter():
         # Set OSR to 32 bits of 1s for future shifting out to intialize pindirs,
-        # y, y again, and x. This requires CHANNELS + 8 + 10 + CHANNELS bits
-        # (maximum of 32 bits for CHANNELS = 7).
+        # y, y again, and x. This requires SENSOR_COUNT + 8 + 10 + SENSOR_COUNT
+        # bits (maximum of 32 bits for SENSOR_COUNT = 7).
         mov(osr, invert(null))
 
         # Set pindirs to 1s to enable output and start charging
         # the capacitor.
-        out(pindirs, NUM_SENSORS)
+        out(pindirs, SENSOR_COUNT)
 
         # Charge up the capacitors for ~32us.
         # Set Y counter to 255 by pulling another 8 bits from OSR.
@@ -41,17 +41,17 @@ class QTRSensors:
         out(y, 10)
 
         # Initialize X (last pin state) to 1s.
-        out(x, NUM_SENSORS)
+        out(x, SENSOR_COUNT)
 
         # Set pins back to inputs by writing 0s to pindirs.
         mov(osr, null)
-        out(pindirs, NUM_SENSORS)
+        out(pindirs, SENSOR_COUNT)
 
         # loop is 8 instructions long = 1us
         label("loop")
 
         # read pins into ISR
-        in_(pins, NUM_SENSORS)
+        in_(pins, SENSOR_COUNT)
 
         # save y in OSR
         mov(osr, y)
@@ -87,11 +87,11 @@ class QTRSensors:
         wrap()
 
     def __init__(self, id, pin1):
-        for i in range(NUM_SENSORS):
+        for i in range(SENSOR_COUNT):
             Pin(pin1+i, Pin.IN, pull=None)
 
         p = Pin(pin1, Pin.OUT, value=1)
-        for i in range(1, NUM_SENSORS):
+        for i in range(1, SENSOR_COUNT):
             Pin(pin1+i, Pin.OUT, value=1)
 
         self.sm = rp2.StateMachine(id, self.counter, freq=8000000, in_base=p, out_base=p)
