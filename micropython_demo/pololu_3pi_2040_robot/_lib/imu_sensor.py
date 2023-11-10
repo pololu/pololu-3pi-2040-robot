6d2acd8b6241b56e6a1bf72e4d710c6c70e24a56
Kevin (Pololu) <dev-kevin@pololu.com>
2023-11-10 15:57:50 -0800

    imu_sensor memory optimization

--- micropython_demo/pololu_3pi_2040_robot/_lib/imu_sensor.py
+++ micropython_demo/pololu_3pi_2040_robot/_lib/imu_sensor.py
@@ -1,9 +1,11 @@
+import array
 import struct
 
 class IMUSensor:
     def __init__(self, i2c, addr):
         self.i2c = i2c
         self.addr = addr
+        self._buf = array.array('h', [0, 0, 0])
 
     def _read_reg(self, reg):
         return self.i2c.readfrom_mem(self.addr, reg, 1)[0]
@@ -15,5 +17,5 @@ class IMUSensor:
         self._write_reg(reg, (self._read_reg(reg) & ~mask) | (val & mask))
 
     def _read_axes_s16(self, first_reg):
-        buf = self.i2c.readfrom_mem(self.addr, first_reg, 6)
-        return list(struct.unpack('<3h', buf))
+        self.i2c.readfrom_mem_into(self.addr, first_reg, self._buf)
+        return list(struct.unpack('<3h', self._buf))
