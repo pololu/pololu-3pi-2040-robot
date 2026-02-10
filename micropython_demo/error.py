pololu/zumo-2040-robot

Main menu moved to main_menu.py; main.py is reserved for user code and will be run by default if it exists.
Demos now allow exiting and returning to the menu, normally with button B.

micropython_demo/error.py
@@ -1,5 +1,23 @@
-# This demo just raises an exception.  When you run it from the
-# default main.py, the exception text will print to the screen, and
-# the buzzer will sound a low tone.
+# This demo shows when happens when you raise an exception. When you
+# run it from main_menu.py, the exception text will print to the screen,
+# and the buzzer will sound a low tone.
 
-raise Exception("This is a test exception.")
+from zumo_2040_robot import robot
+
+display = robot.Display()
+buttonA = robot.ButtonA()
+buttonB = robot.ButtonB()
+
+while True:
+    display.fill_rect(0, 0, 128, 18, 0)
+    display.text("Press A to raise", 0, 0)
+    display.text("exception", 0, 8)
+    display.text("Press B to exit", 0, 56)
+    display.show()
+
+    if buttonA.check():
+        raise Exception("This is a test exception.")
+    if buttonB.check():
+        break
+
+

micropython_demo/zumo_2040_robot/extras/splash_loader.py
@@ -1,7 +1,6 @@
 def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
     from zumo_2040_robot.display import Display
     display = Display()
-    splash = None
     if splash_delay_s:
         # Display the splash screen ASAP.
         splash = display.load_pbm("zumo_2040_robot/extras/splash.pbm")
@@ -13,38 +12,62 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
     button_b_beep = "!e32"
     button_c_beep = "!g32"
 
-    from zumo_2040_robot.buttons import ButtonA, ButtonB, ButtonC
-    from zumo_2040_robot.buzzer import Buzzer
-    from zumo_2040_robot.battery import Battery
-    from zumo_2040_robot.rgb_leds import RGBLEDs
-    from zumo_2040_robot.yellow_led import YellowLED
-    import time
-    from machine import PWM
+    button_a = None
+    button_b = None
+    button_c = None
+    battery = None
+    buzzer = None
+    rgb_leds = None
+    pwm = None
+    selected_menu_option = 0
 
-    button_a = ButtonA()
-    button_b = ButtonB()
-    button_c = ButtonC()
-    battery = Battery()
-    buzzer = Buzzer() # turns off buzzer
-    rgb_leds = RGBLEDs() # turns off RGB LEDs
-    rgb_leds.set_brightness(4)
+    def init_vars():
+        from zumo_2040_robot.motors import Motors
+        Motors()  # turn off motors ASAP
 
-    # Initialize a PWM on the yellow LED at 0% duty cycle, which
-    # corresponds to full brightness.  We can't initialize PWM with
-    # the LED off so we'll just have it on for a while.
-    pwm = PWM(YellowLED().pin)
-    pwm.freq(1000)
-    pwm.duty_u16(0)
+        nonlocal button_a, button_b, button_c, battery, buzzer, rgb_leds, pwm
+        nonlocal display
+        if pwm is not None: return
+
+        from zumo_2040_robot.display import Display
+        from zumo_2040_robot.buttons import ButtonA, ButtonB, ButtonC
+        from zumo_2040_robot.buzzer import Buzzer
+        from zumo_2040_robot.battery import Battery
+        from zumo_2040_robot.rgb_leds import RGBLEDs
+        from zumo_2040_robot.yellow_led import YellowLED
+        from machine import PWM
+
+        if display is None: display = Display()
+        button_a = ButtonA()
+        button_b = ButtonB()
+        button_c = ButtonC()
+        battery = Battery()
+        buzzer = Buzzer() # turns off buzzer
+        rgb_leds = RGBLEDs() # turns off RGB LEDs
+        rgb_leds.set_brightness(4)
+
+        # Initialize a PWM on the yellow LED at 0% duty cycle, which
+        # corresponds to full brightness.  We can't initialize PWM with
+        # the LED off so we'll just have it on for a while.
+        pwm = PWM(YellowLED().pin)
+        pwm.freq(1000)
+        pwm.duty_u16(0)
+
+    def leds_off():
+        from zumo_2040_robot.yellow_led import YellowLED
+        nonlocal rgb_leds
+        rgb_leds.off()
+        YellowLED() # turns off the LED even from PWM
 
     def del_vars():
-        nonlocal display, splash, button_a, button_b, button_c, battery
+        nonlocal display, button_a, button_b, button_c, battery
         nonlocal buzzer, rgb_leds, pwm
 
         pwm.deinit()
-        YellowLED() # turn off yellow LED and reset to an output
+        leds_off()
 
-        del display, splash, button_a, button_b, button_c, battery
-        del buzzer, rgb_leds, pwm
+        display = button_a = button_b = button_c = battery = \
+          buzzer = rgb_leds = pwm = None
 
     def read_button():
         if button_a.is_pressed(): return "A"
@@ -53,17 +76,23 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
         return None
 
     def initial_screen():
+        buzzer.play_in_background(welcome_song)
+        button = None
+        import time
         start = time.ticks_ms()
         while True:
             if button_a.is_pressed():
                 buzzer.play_in_background(button_a_beep)
-                return "A"
+                button = "A"
+                break
             if button_b.is_pressed():
                 buzzer.play_in_background(button_b_beep)
-                return "B"
+                button = "B"
+                break
             if button_c.is_pressed():
                 buzzer.play_in_background(button_c_beep)
-                return "C"
+                button = "C"
+                break
 
             elapsed = time.ticks_ms() - start
             countdown_s = splash_delay_s - elapsed//1000
@@ -75,7 +104,7 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
                 offset = 0
             else:
                 offset = max(-32, -32 * (elapsed - 1000) // 400)
-            display.blit(splash, 0, offset)
+            if splash: display.blit(splash, 0, offset)
             display.text('Push C for menu', 0, 68+offset)
             display.text(f"Default ({countdown_s}s):", 0, 78+offset)
 
@@ -100,12 +129,12 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
                     rgb_leds.set(4-i, [0, 0, 0])
                     rgb_leds.set((4+i)%6, [0, 0, 0])
             rgb_leds.show()
-
-        return None
+        rgb_leds.off()
+        return button
 
     def run_file(filename):
-        rgb_leds.off()
-        YellowLED() # turns off the LED even from PWM
+        import time
+        leds_off()
         if run_file_delay_ms:
             display.fill(0)
             display.text('Run file:', 0, 0)
@@ -121,8 +150,8 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
 
     # Runs the bootloader in the RP2040's Bootrom.
     def run_bootloader():
-        rgb_leds.off()
-        YellowLED() # turns off the LED even from PWM
+        import time
+        leds_off()
         if run_file_delay_ms:
             display.fill(0)
             display.text('Bootloader...', 0, 0)
@@ -134,12 +163,12 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
         buzzer.off()
         machine.bootloader()
 
-    # Runs the Python REPL using sys.exit().
-    # You can also run the REPL by letting the interpreter reach the end of
-    # your program or by sending Ctrl+C to the USB virtual serial port.
+    # Notifies the user that we are about to run the REPL.
+    # After returning, we run the REPL by allowing program to end.
+    # You can also run the REPL by sending Ctrl+C to USB.
     def run_repl():
-        rgb_leds.off()
-        YellowLED() # turns off the LED even from PWM
+        import time
+        leds_off()
         if run_file_delay_ms:
             display.fill(0)
             display.text('exit to REPL...', 0, 0)
@@ -147,21 +176,26 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
             time.sleep_ms(run_file_delay_ms)
         display.fill(0)
         display.show()
-        import sys
-        sys.exit(0)
 
     def menu():
-        rgb_leds.off()
+        run_menu = True
+        while run_menu:
+            run_menu = run_menu_once()
 
+    def run_menu_once():
+        init_vars()
         from zumo_2040_robot.extras.menu import Menu
         import os
+        import time
         from math import exp
+        nonlocal selected_menu_option
 
         start_ms = time.ticks_ms()
-        options = sorted(f for f in os.listdir() if f.endswith(".py") and f != "main.py")
+        options = sorted(f for f in os.listdir() if f.endswith(".py") and f != "main_menu.py")
         options += ["bootloader", "exit to REPL"]
 
         menu = Menu(options)
+        menu.index = selected_menu_option
         menu.display = display
         menu.buzzer = buzzer
         menu.previous_button = button_a
@@ -170,7 +204,7 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
         i = None
         mv = None
 
-        while i == None:
+        while i is None:
             t = time.ticks_ms()
             elapsed_ms = time.ticks_diff(t, start_ms)
 
@@ -189,13 +223,23 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
 
             i = menu.update()
 
+        selected_menu_option = i
         option = options[i]
         if option == "bootloader":
             run_bootloader()
         elif option == "exit to REPL":
             run_repl()
+            return False # do not run again
         else:
             run_file(option)
+            return True # run again
+
+    init_vars()
+
+    import os
+
+    if not(default_program in os.listdir()):
+        default_program = None
 
     if button_a.is_pressed():
         run_file("self_test.py")
@@ -203,8 +247,9 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
     button = read_button()
 
     if button == None and splash_delay_s != 0:
-        buzzer.play_in_background(welcome_song)
         button = initial_screen()
+
+    del splash # done with this var
 
     if button == None:
         if default_program:

pololu/pololu-3pi-2040-robot

micropython_demo/error.py
@@ -1,5 +1,23 @@
-# This demo just raises an exception.  When you run it from the
-# default main.py, the exception text will print to the screen, and
-# the buzzer will sound a low tone.
+# This demo shows when happens when you raise an exception. When you
+# run it from main_menu.py, the exception text will print to the screen,
+# and the buzzer will sound a low tone.
 
-raise Exception("This is a test exception.")
+from pololu_3pi_2040_robot import robot
+
+display = robot.Display()
+buttonA = robot.ButtonA()
+buttonB = robot.ButtonB()
+
+while True:
+    display.fill_rect(0, 0, 128, 18, 0)
+    display.text("Press A to raise", 0, 0)
+    display.text("exception", 0, 8)
+    display.text("Press B to exit", 0, 56)
+    display.show()
+
+    if buttonA.check():
+        raise Exception("This is a test exception.")
+    if buttonB.check():
+        break
+
+

micropython_demo/pololu_3pi_2040_robot/extras/splash_loader.py
@@ -1,7 +1,6 @@
 def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
     from pololu_3pi_2040_robot.display import Display
     display = Display()
-    splash = None
     if splash_delay_s:
         # Display the splash screen ASAP.
         splash = display.load_pbm("pololu_3pi_2040_robot/extras/splash.pbm")
@@ -13,38 +12,62 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
     button_b_beep = "!e32"
     button_c_beep = "!g32"
 
-    from pololu_3pi_2040_robot.buttons import ButtonA, ButtonB, ButtonC
-    from pololu_3pi_2040_robot.buzzer import Buzzer
-    from pololu_3pi_2040_robot.battery import Battery
-    from pololu_3pi_2040_robot.rgb_leds import RGBLEDs
-    from pololu_3pi_2040_robot.yellow_led import YellowLED
-    import time
-    from machine import PWM
+    button_a = None
+    button_b = None
+    button_c = None
+    battery = None
+    buzzer = None
+    rgb_leds = None
+    pwm = None
+    selected_menu_option = 0
 
-    button_a = ButtonA()
-    button_b = ButtonB()
-    button_c = ButtonC()
-    battery = Battery()
-    buzzer = Buzzer() # turns off buzzer
-    rgb_leds = RGBLEDs() # turns off RGB LEDs
-    rgb_leds.set_brightness(4)
+    def init_vars():
+        from pololu_3pi_2040_robot.motors import Motors
+        Motors()  # turn off motors ASAP
 
-    # Initialize a PWM on the yellow LED at 0% duty cycle, which
-    # corresponds to full brightness.  We can't initialize PWM with
-    # the LED off so we'll just have it on for a while.
-    pwm = PWM(YellowLED().pin)
-    pwm.freq(1000)
-    pwm.duty_u16(0)
+        nonlocal button_a, button_b, button_c, battery, buzzer, rgb_leds, pwm
+        nonlocal display
+        if pwm is not None: return
+
+        from pololu_3pi_2040_robot.display import Display
+        from pololu_3pi_2040_robot.buttons import ButtonA, ButtonB, ButtonC
+        from pololu_3pi_2040_robot.buzzer import Buzzer
+        from pololu_3pi_2040_robot.battery import Battery
+        from pololu_3pi_2040_robot.rgb_leds import RGBLEDs
+        from pololu_3pi_2040_robot.yellow_led import YellowLED
+        from machine import PWM
+
+        if display is None: display = Display()
+        button_a = ButtonA()
+        button_b = ButtonB()
+        button_c = ButtonC()
+        battery = Battery()
+        buzzer = Buzzer() # turns off buzzer
+        rgb_leds = RGBLEDs() # turns off RGB LEDs
+        rgb_leds.set_brightness(4)
+
+        # Initialize a PWM on the yellow LED at 0% duty cycle, which
+        # corresponds to full brightness.  We can't initialize PWM with
+        # the LED off so we'll just have it on for a while.
+        pwm = PWM(YellowLED().pin)
+        pwm.freq(1000)
+        pwm.duty_u16(0)
+
+    def leds_off():
+        from pololu_3pi_2040_robot.yellow_led import YellowLED
+        nonlocal rgb_leds
+        rgb_leds.off()
+        YellowLED() # turns off the LED even from PWM
 
     def del_vars():
-        nonlocal display, splash, button_a, button_b, button_c, battery
+        nonlocal display, button_a, button_b, button_c, battery
         nonlocal buzzer, rgb_leds, pwm
 
         pwm.deinit()
-        YellowLED() # turn off yellow LED and reset to an output
+        leds_off()
 
-        del display, splash, button_a, button_b, button_c, battery
-        del buzzer, rgb_leds, pwm
+        display = button_a = button_b = button_c = battery = \
+          buzzer = rgb_leds = pwm = None
 
     def read_button():
         if button_a.is_pressed(): return "A"
@@ -53,17 +76,23 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
         return None
 
     def initial_screen():
+        buzzer.play_in_background(welcome_song)
+        button = None
+        import time
         start = time.ticks_ms()
         while True:
             if button_a.is_pressed():
                 buzzer.play_in_background(button_a_beep)
-                return "A"
+                button = "A"
+                break
             if button_b.is_pressed():
                 buzzer.play_in_background(button_b_beep)
-                return "B"
+                button = "B"
+                break
             if button_c.is_pressed():
                 buzzer.play_in_background(button_c_beep)
-                return "C"
+                button = "C"
+                break
 
             elapsed = time.ticks_ms() - start
             countdown_s = splash_delay_s - elapsed//1000
@@ -75,7 +104,7 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
                 offset = 0
             else:
                 offset = max(-32, -32 * (elapsed - 1000) // 400)
-            display.blit(splash, 0, offset)
+            if splash: display.blit(splash, 0, offset)
             display.text('Push C for menu', 0, 68+offset)
             display.text(f"Default ({countdown_s}s):", 0, 78+offset)
 
@@ -100,12 +129,12 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
                     rgb_leds.set(4-i, [0, 0, 0])
                     rgb_leds.set((4+i)%6, [0, 0, 0])
             rgb_leds.show()
-
-        return None
+        rgb_leds.off()
+        return button
 
     def run_file(filename):
-        rgb_leds.off()
-        YellowLED() # turns off the LED even from PWM
+        import time
+        leds_off()
         if run_file_delay_ms:
             display.fill(0)
             display.text('Run file:', 0, 0)
@@ -121,8 +150,8 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
 
     # Runs the bootloader in the RP2040's Bootrom.
     def run_bootloader():
-        rgb_leds.off()
-        YellowLED() # turns off the LED even from PWM
+        import time
+        leds_off()
         if run_file_delay_ms:
             display.fill(0)
             display.text('Bootloader...', 0, 0)
@@ -134,12 +163,12 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
         buzzer.off()
         machine.bootloader()
 
-    # Runs the Python REPL using sys.exit().
-    # You can also run the REPL by letting the interpreter reach the end of
-    # your program or by sending Ctrl+C to the USB virtual serial port.
+    # Notifies the user that we are about to run the REPL.
+    # After returning, we run the REPL by allowing program to end.
+    # You can also run the REPL by sending Ctrl+C to USB.
     def run_repl():
-        rgb_leds.off()
-        YellowLED() # turns off the LED even from PWM
+        import time
+        leds_off()
         if run_file_delay_ms:
             display.fill(0)
             display.text('exit to REPL...', 0, 0)
@@ -147,21 +176,26 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
             time.sleep_ms(run_file_delay_ms)
         display.fill(0)
         display.show()
-        import sys
-        sys.exit(0)
 
     def menu():
-        rgb_leds.off()
+        run_menu = True
+        while run_menu:
+            run_menu = run_menu_once()
 
+    def run_menu_once():
+        init_vars()
         from pololu_3pi_2040_robot.extras.menu import Menu
         import os
+        import time
         from math import exp
+        nonlocal selected_menu_option
 
         start_ms = time.ticks_ms()
-        options = sorted(f for f in os.listdir() if f.endswith(".py") and f != "main.py")
+        options = sorted(f for f in os.listdir() if f.endswith(".py") and f != "main_menu.py")
         options += ["bootloader", "exit to REPL"]
 
         menu = Menu(options)
+        menu.index = selected_menu_option
         menu.display = display
         menu.buzzer = buzzer
         menu.previous_button = button_a
@@ -170,7 +204,7 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
         i = None
         mv = None
 
-        while i == None:
+        while i is None:
             t = time.ticks_ms()
             elapsed_ms = time.ticks_diff(t, start_ms)
 
@@ -189,13 +223,23 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
 
             i = menu.update()
 
+        selected_menu_option = i
         option = options[i]
         if option == "bootloader":
             run_bootloader()
         elif option == "exit to REPL":
             run_repl()
+            return False # do not run again
         else:
             run_file(option)
+            return True # run again
+
+    init_vars()
+
+    import os
+
+    if not(default_program in os.listdir()):
+        default_program = None
 
     if button_a.is_pressed():
         run_file("self_test.py")
@@ -203,8 +247,9 @@ def splash_loader(*, default_program, splash_delay_s, run_file_delay_ms):
     button = read_button()
 
     if button == None and splash_delay_s != 0:
-        buzzer.play_in_background(welcome_song)
         button = initial_screen()
+
+    del splash # done with this var
 
     if button == None:
         if default_program: